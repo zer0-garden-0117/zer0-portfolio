a0af5e78c414524224731d87c28feac02cb33575
zer0 <zer0@zer0noMac-mini.local>
2025-05-26 01:57:07 +0900

    favicon設定、見出しのデザイン修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <SimpleGrid cols={{ base: 1, sm: 1, md: 1 }} spacing="xl" >
         <ProfileCard />
       </SimpleGrid>
-      <Title order={2} mr={10} mt={10} mb={10} ml={10} c={"var(--mantine-color-dimmed)"}>
+      <Title order={2} mr={10} mt={10} mb={10} ml={10} c={"var(--mantine-color-dimmed)"} size={20} fw={700}>
         Works
       </Title>
       <SimpleGrid cols={{ base: 1, sm: 1, md: 2 }} spacing="md" ml={10} mr={10}>
